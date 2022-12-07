403dce0ff11e4d630dbbaa2959ffb6f212400458
mirhatomoraliev <mirhatomoraliev@gmail.com>
2022-12-07 11:13:45 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        double[] array = {8.2, -64.2, 12.5, 31.0, 16.2, -9.69, 51.0, 6.3,
+                          5.99, -87.2, 13.2, 32.2, 14.2, -8.5, -6.9};
+        int index = 0, count=0;
+        double sum=0;
+        for (double i: array) {
+            index ++;
+            while (i<0) {
+
+//                 for (int j = index; j< array.length;j++){
+//                     if(array[j] > 0){
+//                         count++;
+//                         sum += array[j];
+//                     }
+//                 }
+            }
+
+//            if(i>0){
+//                count ++;
+//            }
+        }
+        System.out.println("Sum= "+ sum);
+    }
+}
