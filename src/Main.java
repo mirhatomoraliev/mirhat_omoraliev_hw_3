2ee959ce8f5666f7d2b8e3ca12e8006d97bb8593
mirhatomoraliev <mirhatomoraliev@gmail.com>
2022-12-07 11:16:45 +0600

    Added comment

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 public class Main {
+    // Hi sensei
     public static void main(String[] args) {
         double[] array = {8.2, -64.2, 12.5, 31.0, 16.2, -9.69, 51.0, 6.3,
                           5.99, -87.2, 13.2, 32.2, 14.2, -8.5, -6.9};
