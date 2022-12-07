d089955dbd2c4e73124e8f807fc7baa5bd11deea
mirhatomoraliev <mirhatomoraliev@gmail.com>
2022-12-07 12:24:37 +0600

    Finished HW

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,49 @@
+import java.util.Arrays;
+
 public class Main {
     // Hi sensei
     public static void main(String[] args) {
         double[] array = {8.2, -64.2, 12.5, 31.0, 16.2, -9.69, 51.0, 6.3,
                           5.99, -87.2, 13.2, 32.2, 14.2, -8.5, -6.9};
-        int index = 0, count=0;
-        double sum=0;
+        int count=0;
+        double sum=0, avg=0;
+        boolean check = false;
+                
         for (double i: array) {
-            index ++;
-            while (i<0) {
-
-//                 for (int j = index; j< array.length;j++){
-//                     if(array[j] > 0){
-//                         count++;
-//                         sum += array[j];
-//                     }
-//                 }
+            if (i<0) {
+                check = true;
             }
-
-//            if(i>0){
-//                count ++;
-//            }
+            if(check){
+                if(i>0) {
+                    count++;
+                    sum += i;
+                }
+            }
         }
-        System.out.println("Sum= "+ sum);
+
+        avg = sum/count;
+        System.out.println("Avarage = "+ avg);
+        System.out.println("----------------------------------");
+
+        sortArray(array);
+
+    }
+    public static void sortArray (double[] array){
+        for (int i = 0; i < array.length; i++) {
+            double min=array[i];
+            int min_i=i;
+            for (int j = i+1; j < array.length; j++) {
+                if(min > array[j]){
+                    min = array[j];
+                    min_i = j;
+                }
+            }
+            if(i != min_i){
+                double tmp = array[i];
+                array[i] = array[min_i];
+                array[min_i] = tmp;
+            }
+        }
+        System.out.println("Sorted array-> "+Arrays.toString(array));
     }
 }
